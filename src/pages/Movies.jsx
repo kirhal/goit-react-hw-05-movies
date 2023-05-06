e1de10a7bba1237704aa1ade444950bd4113f3f3
kirhal <kirillgaleckiy@gmail.com>
2023-05-06 21:33:55 +0200

    movies upd

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,3 +1,23 @@
-export default function Home() {
-  return <div>Movies</div>;
+import { useState, useEffect } from 'react';
+import { Form } from 'react-router-dom';
+import { fetchTrendingDay } from '../instruments/fetchAPI';
+
+export default function Movies() {
+  const [request, setRequest] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  return (
+    <form>
+      <input
+        type="text"
+        autoComplete="off"
+        autoFocus
+        placeholder="Search movies"
+        name="input"
+      ></input>
+      <button type="submit">Search</button>
+    </form>
+  );
 }
